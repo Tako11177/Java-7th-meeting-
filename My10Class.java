b14cb6d6eb17bf61eaea6e14cb731bc9f5d17eef
Tako11177 <takokorinteli7@gmail.com>
2025-05-09 17:11:00 +0400

    Cycles

---
+++ My10Class.java
@@ -0,0 +1,48 @@
+public class My10Class {
+    public static void main(String[] args) {
+
+        /* for ციკლი*/
+        for (int i = 0; i < 5; i++) {
+            System.out.println("i = " + i);
+        }
+        /* პასუხი:
+i = 0
+i = 1
+i = 2
+i = 3
+i = 4
+         */
+
+
+        /* while ციკლი*/
+        int x = 0;
+        while (x < 5) {
+            System.out.println("x = " + x);
+            x++;
+        }
+
+        /* პასუხი:
+x = 0
+x = 1
+x = 2
+x = 3
+x = 4
+         */
+        /* do while ციკლი*/
+        int y = 0;
+        do {
+            System.out.println("y = " + y);
+            y++;
+        } while (y < 5);
+
+        /* პასუხი:
+y = 0
+y = 1
+y = 2
+y = 3
+y = 4
+         */
+
+
+    }
+}
