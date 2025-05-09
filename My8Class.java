8c167bb80d0d99044fdf30fc2cafda19ea69d0b6
Tako11177 <takokorinteli7@gmail.com>
2025-05-09 15:42:10 +0400

    for, count2

---
+++ My8Class.java
@@ -0,0 +1,17 @@
+public class My8Class {
+    public static void main(String[] args) {
+        int count=0;
+
+
+        for (int i = 0; i < 5; i++) {
+            /*System.out.println(i + " java ");*/
+            count++; /* ეს ნიშნავს count გაიზარდოს იმდენჯერ რამდენჯერაც ეს ლოგიკა (int i = 0; i < 5; i++) იქნება True*/
+        }
+        System.out.println("count: " + count);
+    }
+}
+
+//პასუხი:
+/*count: 5*/
+
+
